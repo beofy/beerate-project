adae1ff44c99f8c553ca51e1830f18c389b3fd63
minxiang <294315568@qq.com>
2019-04-11 10:49:19 +0800

    验证码请求校验控制器

---
+++ beerate-security/src/main/java/cn/beerate/controller/CaptchaController.java
@@ -0,0 +1,70 @@
+package cn.beerate.controller;
+
+import cn.beerate.captcha.CaptchaProcessor;
+import cn.beerate.captcha.Captcha;
+import cn.beerate.captcha.CaptchaProcessorHolder;
+import cn.beerate.common.Message;
+import cn.beerate.captcha.CaptchaScene;
+import org.apache.commons.lang3.EnumUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 验证码控制
+ */
+@RestController
+@RequestMapping("/captcha")
+public class CaptchaController {
+
+    /**
+     * 验证码处理容器
+     */
+    @Autowired
+    private CaptchaProcessorHolder captchaProcessorHolder;
+
+    @Autowired
+    private HttpServletRequest requestProvidor;
+
+
+    @GetMapping("/{captcha}/{scene}")
+    public Message<String> create(@PathVariable String captcha, @PathVariable String scene, HttpServletResponse response) throws IOException {
+
+        Captcha captchaEum = EnumUtils.getEnum(Captcha.class,captcha.toUpperCase());
+        Assert.notNull(captchaEum, "验证码类型错误！");
+
+        CaptchaScene sceneEum = EnumUtils.getEnum(CaptchaScene.class,scene.toUpperCase());
+        Assert.notNull(captchaEum, "验证码场景类型错误！");
+
+        //获取验证码处理器
+        CaptchaProcessor captchaProcessor = captchaProcessorHolder.getCaptchaProcessor(captchaEum);
+        Assert.notNull(captchaProcessor, "验证码处理器未找到！");
+
+        //创建验证码
+        return captchaProcessor.create(requestProvidor, response,sceneEum ,captchaEum);
+    }
+
+    @PostMapping("/{captcha}/{scene}")
+    public Message check(@PathVariable String captcha, @PathVariable String scene, @Param("captchaCode") String captchaCode ){
+
+        Captcha captchaEum = EnumUtils.getEnum(Captcha.class,captcha.toUpperCase());
+        Assert.notNull(captchaEum, "验证码类型错误");
+
+        CaptchaScene captchaScene = EnumUtils.getEnum(CaptchaScene.class,scene.toUpperCase());
+        Assert.notNull(captchaEum, "验证码场景类型错误");
+
+        Assert.notNull(captchaCode, "验证码错误");
+
+        //获取验证码处理
+        CaptchaProcessor captchaProcessor = captchaProcessorHolder.getCaptchaProcessor(captchaEum);
+        Assert.notNull(captchaProcessor, "验证码处理器未找到！");
+
+        return captchaProcessor.check(requestProvidor,captchaEum,captchaScene,captchaCode);
+    }
+
+}
