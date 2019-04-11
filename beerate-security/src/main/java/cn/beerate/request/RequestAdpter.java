fe0ef5abbfa221a2c1f3bc6aa09bc76c8216588a
minxiang <294315568@qq.com>
2019-04-11 10:48:21 +0800

    请求适配器 非web请求采用缓存进行会话管理

---
+++ beerate-security/src/main/java/cn/beerate/request/RequestAdpter.java
@@ -0,0 +1,19 @@
+package cn.beerate.request;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 请求适配器
+ */
+interface RequestAdpter extends HttpServletRequest {
+    /**
+     * 访问渠道
+     */
+    String CHANNEL = "channel";
+
+    /**
+     * 唯一标识码
+     */
+    String CHANNELID = "channelId";
+
+}
