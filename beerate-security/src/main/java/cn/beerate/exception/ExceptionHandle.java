2c5f19350452286899f460094c4c36637862c73c
minxiang <294315568@qq.com>
2019-04-11 10:47:39 +0800

    异常处理器

--- beerate-security/src/main/java/cn/beerate/exception/ExceptionHandle.java
+++ beerate-security/src/main/java/cn/beerate/exception/ExceptionHandle.java
@@ -1,6 +1,8 @@
 package cn.beerate.exception;
 
 import cn.beerate.common.Message;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @ControllerAdvice
 @RestController
-public class ExceptionController {
+public class ExceptionHandle {
+
+    private static final Log logger = LogFactory.getLog(ExceptionHandle.class);
+
     /**
      * 异常处理
      */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.OK)
     public Message handleException(Exception ex) {
+        logger.error("系统异常",ex);
         return Message.error(ex.getMessage());
     }
 
