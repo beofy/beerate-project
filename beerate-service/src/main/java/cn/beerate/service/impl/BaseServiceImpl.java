6b5ea6c0ba01b7ec528ef60a6f18422f8503614d
minxiang <294315568@qq.com>
2019-04-19 15:00:26 +0800

    基础共用业务实现类

--- beerate-service/src/main/java/cn/beerate/service/impl/BaseServiceImpl.java
+++ beerate-service/src/main/java/cn/beerate/service/impl/BaseServiceImpl.java
@@ -1,7 +1,29 @@
 package cn.beerate.service.impl;
 
+import cn.beerate.dao.IBaseDao;
+import cn.beerate.model.entity.Model;
 import cn.beerate.service.IBaseService;
 
-public class BaseServiceImpl implements IBaseService {
+public class BaseServiceImpl<T extends Model> implements IBaseService<T> {
+    private IBaseDao<T> iBaseDao;
 
+    <D extends IBaseDao<T>> BaseServiceImpl(D d) {
+        iBaseDao=d;
+    }
+
+    @Override
+    public T getOne(long id) {
+        return iBaseDao.getOne(id);
+    }
+
+    @Override
+    public T delete(T t) {
+        iBaseDao.delete(t);
+        return t;
+    }
+
+    @Override
+    public T save(T t) {
+        return iBaseDao.save(t);
+    }
 }
