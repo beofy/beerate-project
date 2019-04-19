3b3d5448381aa7d1f7dc0e13346d3f99c5801048
minxiang <294315568@qq.com>
2019-04-19 14:58:37 +0800

    前后台用户共用的model

--- beerate-dao/src/main/java/cn/beerate/model/entity/UserAndAdminModel.java
+++ beerate-dao/src/main/java/cn/beerate/model/entity/UserAndAdminModel.java
@@ -11,18 +11,18 @@ import javax.persistence.MappedSuperclass;
 @NoArgsConstructor
 @Setter
 @Getter
-class UserAndAdminModel extends Model{
+public class UserAndAdminModel extends Model{
 
     @Column(columnDefinition = "varchar(50) not null default '' comment '用户名'")
     private String username;
 
-    @Column(columnDefinition = "varchar(15) not null default '' comment '手机号'")
+    @Column(columnDefinition = "varchar(20) not null default '' comment '手机号'")
     private String mobile;
 
-    @Column(columnDefinition = "varchar(15) not null default '' comment '邮箱'")
+    @Column(columnDefinition = "varchar(64) not null default '' comment '邮箱'")
     private String email;
 
-    @Column(columnDefinition = "varchar(15) not null default '' comment '密码'")
+    @Column(columnDefinition = "varchar(128) not null default '' comment '密码'")
     private String password;
 
 }
