9814ccb2d1526eeb18f93e901f457c241bf437d1
minxiang <294315568@qq.com>
2019-04-19 14:59:41 +0800

    修改基础service业务类

--- beerate-service/src/main/java/cn/beerate/service/IBaseService.java
+++ beerate-service/src/main/java/cn/beerate/service/IBaseService.java
@@ -1,4 +1,26 @@
 package cn.beerate.service;
 
-public interface IBaseService {
+import cn.beerate.model.entity.Model;
+
+/**
+ * 基础业务接口
+ */
+public interface IBaseService<T extends Model> {
+
+
+    /**
+     * 根据主键id查询
+     */
+    T getOne(long id);
+
+    /**
+     * 删除
+     */
+    T delete(T t);
+
+    /**
+     * 修改
+     */
+    T save(T t);
+
 }
