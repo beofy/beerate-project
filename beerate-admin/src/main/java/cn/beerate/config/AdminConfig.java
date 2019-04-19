8fdb99b74c90c157f93cf001a3412622f9df562a
minxiang <294315568@qq.com>
2019-04-19 15:01:39 +0800

    后台配置

---
+++ beerate-admin/src/main/java/cn/beerate/config/AdminConfig.java
@@ -0,0 +1,26 @@
+package cn.beerate.config;
+
+import cn.beerate.interceptor.AdminLoginInterceptor;
+import cn.beerate.interceptor.AdminRightInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AdminConfig implements WebMvcConfigurer {
+
+    private AdminLoginInterceptor adminLoginInterceptor;
+    private AdminRightInterceptor adminRightInterceptor;
+
+    public AdminConfig(AdminLoginInterceptor adminLoginInterceptor, AdminRightInterceptor adminRightInterceptor) {
+        this.adminLoginInterceptor = adminLoginInterceptor;
+        this.adminRightInterceptor = adminRightInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminLoginInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/login.html","/admin/login");
+
+        registry.addInterceptor(adminRightInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/login.html","/admin/login");
+    }
+}
