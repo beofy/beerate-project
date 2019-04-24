a53c0fa795e3dba5715b4b233aaf2a21e85b6f2a
minxiang <294315568@qq.com>
2019-04-24 16:56:16 +0800

    异常返回，根据URL区分前后台消息通知

--- beerate-security/src/main/java/cn/beerate/exception/ExceptionHandle.java
+++ beerate-security/src/main/java/cn/beerate/exception/ExceptionHandle.java
@@ -1,11 +1,10 @@
 package cn.beerate.exception;
 
 import cn.beerate.common.Message;
-import cn.beerate.request.ChannelType;
-import cn.beerate.session.CacheSeesion;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,9 +30,16 @@ public class ExceptionHandle {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.OK)
     public Object handleException(Exception ex) {
-        logger.error("系统异常",ex);
-        CacheSeesion cacheSeesion = (CacheSeesion)request.getSession();
-        if(cacheSeesion.getChannel().equalsIgnoreCase(ChannelType.WEB.name())){
+        logger.error(String.format("系统异常：[%s],原因：[%s]",ex.getMessage(),ex.getCause()));
+        /* AntPathMatcher
+         * 匹配大小写敏感，可用通配符为:?,*,**
+         * ?表示单个字符
+         * *表示一层路径内的任意字符串，不可跨层级
+         * **表示任意层路径
+         */
+
+        //匹配后台请求
+        if(new AntPathMatcher().match("/admin/**",request.getRequestURI())){
             ModelAndView modelAndView = new ModelAndView();
             modelAndView.setViewName("error");
             return modelAndView;
