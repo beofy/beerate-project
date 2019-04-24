373f42f0a985a3737077d7c16423f5c00d753a66
minxiang <294315568@qq.com>
2019-04-24 15:46:32 +0800

    修改用户实体

--- beerate-user/src/main/java/cn/beerate/model/entity/t_user.java
+++ beerate-user/src/main/java/cn/beerate/model/entity/t_user.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.Date;
 
@@ -15,32 +16,34 @@ import java.util.Date;
 @org.hibernate.annotations.Table(appliesTo = "t_user",comment="前台用户表")
 public class t_user extends UserAndAdminModel{
 
-    /** 连续登录失败次数 */
-    private int password_continue_fails;
+    @Column(columnDefinition = "int not null default 0 comment '密码连续错误次数'")
+    private Integer password_continue_fails;
 
-    /** 密码连续错误被锁定 */
-    private boolean is_password_locked;
-
-    /** 密码连续错误被锁定时间 */
+    @Column(columnDefinition = "datetime comment '密码锁定时间'")
     private Date password_locked_time;
 
-    /** 是否允许登录(锁定) */
-    private boolean is_allow_login;
+    @Column(columnDefinition = "bit not null default 0 comment '是否允许登录'")
+    private Boolean is_allow_login;
 
-    /** 后台管理员执行锁定的时间 */
+    @Column(columnDefinition = "datetime comment '锁定时间'")
     private Date lock_time;
 
-    /** 登录次数 */
-    private int login_count;
+    @Column(columnDefinition = "int comment '登录次数'")
+    private Integer login_count;
 
-    /** 上次登录时间 */
+    @Column(columnDefinition = "datetime comment '最后一次登录时间'")
     private Date last_login_time;
 
-    /** 上次登录ip */
+    @Column(columnDefinition = "varchar(20) default '' comment '最后一次登录ip'")
     private String last_login_ip;
 
-    /** 上次登录入口(枚举)：1 pc 2 app 3 wechat */
-    private int last_login_client;
+    @Column(columnDefinition = "varchar(20) default '' comment '最后一次登录渠道'")
+    private String last_login_client;
 
+    @Column(columnDefinition = "varchar(20) default '' comment '注册ip'")
+    private String registe_ip;
+
+    @Column(columnDefinition = "varchar(20) default '' comment '注册渠道'")
+    private String registe_channel;
 
 }
