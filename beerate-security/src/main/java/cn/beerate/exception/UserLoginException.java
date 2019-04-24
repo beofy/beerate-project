d717c431be065b099d5d734ad1b30f72e37a4cd4
minxiang <294315568@qq.com>
2019-04-24 15:49:10 +0800

    用户登录拦截

---
+++ beerate-security/src/main/java/cn/beerate/exception/UserLoginException.java
@@ -0,0 +1,12 @@
+package cn.beerate.exception;
+
+/**
+ * 用户登录异常
+ */
+public class UserLoginException extends Exception{
+
+    public UserLoginException(String message) {
+        super(message);
+    }
+
+}
