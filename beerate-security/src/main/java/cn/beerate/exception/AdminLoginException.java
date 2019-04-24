ba3933b7bffcc232c71f89ba556a3602720f79c7
minxiang <294315568@qq.com>
2019-04-24 15:48:50 +0800

    update

---
+++ beerate-security/src/main/java/cn/beerate/exception/AdminLoginException.java
@@ -0,0 +1,11 @@
+package cn.beerate.exception;
+
+/**
+ * 管理员登录异常
+ */
+public class AdminLoginException extends Exception{
+
+    public AdminLoginException(String message) {
+        super(message);
+    }
+}
