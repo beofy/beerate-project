6b0852e5a2990766cb58d3d37034599ebcacf237
minxiang <294315568@qq.com>
2019-04-24 10:41:57 +0800

    添加通用分页方法

--- beerate-dao/src/main/java/cn/beerate/dao/IBaseDao.java
+++ beerate-dao/src/main/java/cn/beerate/dao/IBaseDao.java
@@ -2,11 +2,12 @@ package cn.beerate.dao;
 
 import cn.beerate.model.entity.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 
 @NoRepositoryBean
-public interface IBaseDao<T extends Model> extends JpaRepository<T,Long> {
+public interface IBaseDao<T extends Model> extends JpaRepository<T,Long>, JpaSpecificationExecutor<T> {
 
 
 }
--- beerate-service/src/main/java/cn/beerate/service/IBaseService.java
+++ beerate-service/src/main/java/cn/beerate/service/IBaseService.java
@@ -1,6 +1,11 @@
 package cn.beerate.service;
 
 import cn.beerate.model.entity.Model;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 
 /**
  * 基础业务接口
@@ -22,5 +27,9 @@ public interface IBaseService<T extends Model> {
      * 修改
      */
     T save(T t);
+
+    Page<T> page(int page,int size,String colum,String order, Example<T> example);
+
+    Page<T> page(int page,int size,String colum,String order,@Nullable Specification<T> spec);
 
 }
