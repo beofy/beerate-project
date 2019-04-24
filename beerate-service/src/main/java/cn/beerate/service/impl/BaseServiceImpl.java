913e486a6e7373ba57da793985d137655ad0d174
minxiang <294315568@qq.com>
2019-04-24 10:46:55 +0800

    实现简单和复杂分页通用方法

--- beerate-service/src/main/java/cn/beerate/service/impl/BaseServiceImpl.java
+++ beerate-service/src/main/java/cn/beerate/service/impl/BaseServiceImpl.java
@@ -3,6 +3,9 @@ package cn.beerate.service.impl;
 import cn.beerate.dao.IBaseDao;
 import cn.beerate.model.entity.Model;
 import cn.beerate.service.IBaseService;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 
 public class BaseServiceImpl<T extends Model> implements IBaseService<T> {
     private IBaseDao<T> iBaseDao;
@@ -26,4 +29,33 @@ public class BaseServiceImpl<T extends Model> implements IBaseService<T> {
     public T save(T t) {
         return iBaseDao.save(t);
     }
+
+    /**
+     * 简单分页查询
+     * <br> 字符类型支持模糊查询
+     * <br>非字符类型只能精准查询
+     * <br>不支持范围查询
+     */
+    public Page<T> page(int page,int size,String colum,String order,Example<T> example){
+        //排序条件
+        Sort sort =Sort.by(Sort.Direction.fromString(order),colum);
+        //分页排序
+        Pageable pageable = PageRequest.of(page-1,size,sort);
+
+        return iBaseDao.findAll(example,pageable);
+    }
+
+    /**
+     * 复杂分页查询
+     * <br> 支持模糊查询
+     * <br> 返回查询
+     */
+    public Page<T> page(int page,int size,String colum,String order,@Nullable Specification<T> spec){
+        //排序条件
+        Sort sort =Sort.by(Sort.Direction.fromString(order),colum);
+        //分页排序
+        Pageable pageable = PageRequest.of(page-1,size,sort);
+        return iBaseDao.findAll(spec,pageable);
+    }
+
 }
