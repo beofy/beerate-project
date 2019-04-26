c037416b34296b9eb7b65ad943d81918d2b24409
minxiang <294315568@qq.com>
2019-04-26 11:55:49 +0800

    添加一个项目启动引导模块

---
+++ beerate-bootstrap/src/main/java/cn/beerate/Bootstrap.java
@@ -0,0 +1,46 @@
+package cn.beerate;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class,args);
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("巴雷特接口文档")
+                .description("文档描述~~")
+                .termsOfServiceUrl("NO terms of service")
+                .contact(new Contact("zhangminxiang","none","294315568@qq.com"))
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                //扫描指定包中的swagger注解
+                .apis(RequestHandlerSelectors.basePackage("cn.beerate.controller"))
+                //扫描所有有注解的api，用这种方式更灵活
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
