e568579ab588870b14a88dfca839aa69a82ddc74
minxiang <294315568@qq.com>
2019-04-29 13:48:53 +0800

    添加一个项目类型通用枚举类

---
+++ beerate-core/src/main/java/cn/beerate/model/ItemType.java
@@ -0,0 +1,29 @@
+package cn.beerate.model;
+
+/**
+ * 项目类型
+ */
+public enum ItemType {
+
+
+    /**
+     * 老股转让
+     */
+    STOCK_TRANSFER,
+
+    /**
+     * 老股转让
+     */
+    PRE_IPO,
+
+    /**
+     * 老股转让
+     */
+    BLOCK_TRADE,
+
+    /**
+     * 老股转让
+     */
+    STOCK_PLEDGE
+
+}
