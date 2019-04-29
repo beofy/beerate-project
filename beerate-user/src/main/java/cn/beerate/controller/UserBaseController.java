d7000f92eb9ba95573abd0574239d527b0c917f1
minxiang <294315568@qq.com>
2019-04-29 13:47:04 +0800

    添加一些用户操作方法

--- beerate-user/src/main/java/cn/beerate/controller/UserBaseController.java
+++ beerate-user/src/main/java/cn/beerate/controller/UserBaseController.java
@@ -1,5 +1,35 @@
 package cn.beerate.controller;
 
+import cn.beerate.constant.SessionKey;
+import cn.beerate.model.bean.User;
+
 public class UserBaseController extends BaseController{
 
+    /**
+     * 获取当前登录管理员
+     */
+    public User getUser(){
+        return getSessionAttr(SessionKey.USER_SESSION_KEY,User.class);
+    }
+
+    /**
+     * 获取当前管理员id
+     */
+    long getUserId(){
+        return getUser().getId();
+    }
+
+    /**
+     * 获取管理员用户名
+     */
+    public String getUserName(){
+        return getUser().getName();
+    }
+
+    /**
+     * 获取用户头像
+     */
+    public String getAdminPhoto(){
+        return getUser().getPhoto();
+    }
 }
