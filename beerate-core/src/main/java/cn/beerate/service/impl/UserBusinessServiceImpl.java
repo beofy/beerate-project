b444a6fa65d5d66be25d9645ece0f349e5d4b8a1
minxiang <294315568@qq.com>
2019-04-29 18:24:11 +0800

    名片识别接口，业务类接口待完善

--- beerate-core/src/main/java/cn/beerate/service/impl/UserBusinessServiceImpl.java
+++ beerate-core/src/main/java/cn/beerate/service/impl/UserBusinessServiceImpl.java
@@ -1,11 +1,13 @@
 package cn.beerate.service.impl;
 
-import cn.beerate.utils.BusinessParseUtil;
+import cn.beerate.model.bean.Business;
+import cn.beerate.utils.BcrUtil;
 import cn.beerate.common.Message;
 import cn.beerate.dao.UserBusinessDao;
 import cn.beerate.model.entity.t_user_business;
 import cn.beerate.service.UserBusinessService;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,17 +24,54 @@ public class UserBusinessServiceImpl extends BaseServiceImpl<t_user_business> im
     }
 
     @Override
-    public Message<String> uploadUserBusiness(String tempFilePath, String userFilePath) {
+    public Message<String> uploadUserBusiness(String tempFilePath, String userFilePath,long userId) {
         File tempFile = new File(tempFilePath);
-        String result = BusinessParseUtil.parse(tempFile);
-        JSONObject
+
+        String result = BcrUtil.parse(tempFile);
+        Business business = JSONObject
                 .parseObject(result)
                 .getJSONArray("outputs")
                 .getJSONObject(0)
                 .getJSONObject("outputValue")
-                .getJSONObject("dataValue")
+                .getJSONObject("dataValue").toJavaObject(Business.class);
 
-        ;
+        t_user_business userBusiness = t_user_business.getInstance();
+
+        if(StringUtils.isNotBlank(business.getName())){
+            userBusiness.setName(business.getName());
+        }
+
+        if(StringUtils.isNotBlank(business.getCompany().get(0))){
+            userBusiness.setCompany(business.getCompany().get(0));
+        }
+
+        if(StringUtils.isNotBlank(business.getDepartment().get(0))){
+            userBusiness.setDepartment(business.getDepartment().get(0));
+        }
+
+        if(StringUtils.isNotBlank(business.getTitle().get(0))){
+            userBusiness.setTitle(business.getTitle().get(0));
+        }
+
+        if(StringUtils.isNotBlank(business.getTel_cell().get(0))){
+            userBusiness.setTelCell(business.getTel_cell().get(0));
+        }
+
+        if(StringUtils.isNotBlank(business.getTel_work().get(0))){
+            userBusiness.setTelWork(business.getTel_work().get(0));
+        }
+
+        if(StringUtils.isNotBlank(business.getEmail().get(0))){
+            userBusiness.setEmail(business.getEmail().get(0));
+        }
+
+        if(StringUtils.isNotBlank(business.getAddr().get(0))){
+            userBusiness.setAddress(business.getAddr().get(0));
+        }
+
+        //设置用户id
+        userBusiness.getUser().setId(userId);
+        //保存名片文件
 
         return null;
     }
