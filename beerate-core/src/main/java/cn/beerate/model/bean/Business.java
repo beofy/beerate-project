b2cf452f7c0635669a7dbb576f6b78c34ae318d6
minxiang <294315568@qq.com>
2019-04-29 15:34:23 +0800

    添加business  bean对象

---
+++ beerate-core/src/main/java/cn/beerate/model/bean/Business.java
@@ -0,0 +1,23 @@
+package cn.beerate.model.bean;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+public class Business {
+    private String name;
+    private String request_id;
+    private Boolean success;
+    private List<String> addr;
+    private List<String> company;
+    private List<String> department;
+    private List<String> email;
+    private List<String> tel_cell;
+    private List<String> tel_work;
+    private List<String> title;
+
+
+}
