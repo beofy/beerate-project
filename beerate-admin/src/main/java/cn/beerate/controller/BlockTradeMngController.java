1cc38b5cb4d07b796c0c19625cd35e25793de10f
minxiang <294315568@qq.com>
2019-05-07 15:58:21 +0800

    初步完善大宗交易列表页

--- beerate-admin/src/main/java/cn/beerate/controller/BlockTradeMngController.java
+++ beerate-admin/src/main/java/cn/beerate/controller/BlockTradeMngController.java
@@ -1,12 +1,22 @@
 package cn.beerate.controller;
 
 import cn.beerate.common.Message;
+import cn.beerate.model.entity.t_admin;
 import cn.beerate.model.entity.t_item_block_trade;
 import cn.beerate.service.BlockTradeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * 后台大宗交易管理-控制器
@@ -24,7 +34,29 @@ public class BlockTradeMngController  extends AdminBaseController {
      * 列表页
      */
     @GetMapping("/list.html")
-    public String listPage(){
+    public String listPage(int page, int size, String column, String order, Date beginDate, Date endDate, t_item_block_trade blockTrade , Model model){
+
+        Page<t_item_block_trade> pageBean = blockTradeService.page(page, size, column, order, new Specification<t_item_block_trade>() {
+            @Override
+            public Predicate toPredicate(Root<t_item_block_trade> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+                List<Predicate> predicates = new ArrayList<>();
+
+                Join<t_item_block_trade,t_admin> adminJoin = root.join("admin", JoinType.LEFT);
+                criteriaBuilder.equal(adminJoin.get("id"),getAdminId());
+
+                if(beginDate!=null&&endDate!=null&&beginDate.before(endDate)){
+                    predicates.add(criteriaBuilder.between(root.get("createTime") , beginDate, endDate));
+                }
+
+                Predicate[] predicate = new Predicate[predicates.size()];
+
+                return criteriaBuilder.and(predicates.toArray(predicate));
+            }
+        });
+
+
+        model.addAttribute("page",pageBean);
 
         return "blocktrade/list";
     }
@@ -41,6 +73,7 @@ public class BlockTradeMngController  extends AdminBaseController {
      * 项目添加
      */
     @PostMapping("/add")
+    @ResponseBody
     public Message<String> add(t_item_block_trade blockTrade){
 
         Message<t_item_block_trade> message = blockTradeService.addBlockTradeByAdmin(blockTrade,getAdminId());
--- beerate-core/src/main/java/cn/beerate/service/impl/BlockTradeServiceImpl.java
+++ beerate-core/src/main/java/cn/beerate/service/impl/BlockTradeServiceImpl.java
@@ -4,7 +4,9 @@ import cn.beerate.common.Message;
 import cn.beerate.dao.BlockTradeDao;
 import cn.beerate.model.AuditStatus;
 import cn.beerate.model.ModelValidate;
+import cn.beerate.model.entity.t_admin;
 import cn.beerate.model.entity.t_item_block_trade;
+import cn.beerate.model.entity.t_user;
 import cn.beerate.service.BlockTradeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +24,7 @@ public class BlockTradeServiceImpl extends BaseServiceImpl<t_item_block_trade> i
     /**
      * 添加大宗交易
      */
+    @Transactional
     public Message<t_item_block_trade> addBlockTrade(t_item_block_trade blockTrade){
         //参数校验
         Message<String> messageValid = ModelValidate.blockTradeValid(blockTrade);
@@ -36,8 +39,12 @@ public class BlockTradeServiceImpl extends BaseServiceImpl<t_item_block_trade> i
     /**
      *  添加大宗交易-前台用户
      */
+    @Transactional
     public Message<t_item_block_trade> addBlockTradeByUser(t_item_block_trade blockTrade,long userId){
-        blockTrade.getUser().setId(userId);
+        t_user user = new t_user();
+        user.setId(userId);
+
+        blockTrade.setUser(user);
         blockTrade.setAuditStatus(AuditStatus.WAIT_AUDIT);//设置审核状态-等待审核
 
         return addBlockTrade(blockTrade);
@@ -46,8 +53,12 @@ public class BlockTradeServiceImpl extends BaseServiceImpl<t_item_block_trade> i
     /**
      *  添加大宗交易-后台管理员
      */
+    @Transactional
     public Message<t_item_block_trade> addBlockTradeByAdmin(t_item_block_trade blockTrade,long adminId){
-        blockTrade.getAdmin().setId(adminId);
+        t_admin admin = new t_admin();
+        admin.setId(adminId);
+
+        blockTrade.setAdmin(admin);
         blockTrade.setAuditStatus(AuditStatus.PASS_AUDIT);//管理员直接通过审核
 
         return addBlockTrade(blockTrade);
