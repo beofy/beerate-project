cadaeca8a4cdd6c3a0ac3b557c8437bee53a85c3
minxiang <294315568@qq.com>
2019-05-07 18:23:20 +0800

    添加老股转让页面

--- beerate-admin/src/main/java/cn/beerate/controller/StockTransferMngController.java
+++ beerate-admin/src/main/java/cn/beerate/controller/StockTransferMngController.java
@@ -1,9 +1,11 @@
 package cn.beerate.controller;
 
 import cn.beerate.common.Message;
+import cn.beerate.model.IndustryRealm;
 import cn.beerate.model.entity.t_item_stock_transfer;
 import cn.beerate.service.StockTransferSerivce;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,7 +33,8 @@ public class StockTransferMngController extends AdminBaseController  {
      * 添加页面
      */
     @GetMapping("/add.html")
-    public String addPage(){
+    public String addPage(Model model){
+        model.addAttribute("industryRealms", IndustryRealm.values());
         return "stocktransfer/add";
     }
 
