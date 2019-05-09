d7aa89b7f8e0a58f4cd30af11035c4893e1e4d7a
minxiang <294315568@qq.com>
2019-05-09 17:39:14 +0800

    数据传输对象

---
+++ beerate-core/src/main/java/cn/beerate/model/bean/BlockTrade.java
@@ -0,0 +1,19 @@
+package cn.beerate.model.bean;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+public class BlockTrade {
+
+    public BlockTrade(String blockTradeName, String stockCode) {
+        this.blockTradeName = blockTradeName;
+        this.stockCode = stockCode;
+    }
+
+    private String blockTradeName;
+
+    private String stockCode;
+
+}
---
+++ beerate-core/src/main/java/cn/beerate/model/bean/ItemLoan.java
@@ -0,0 +1,16 @@
+package cn.beerate.model.bean;
+
+import lombok.Data;
+
+@Data
+public class ItemLoan {
+
+    private String itemName;
+
+    private String companyName;
+
+    private String logoUri;
+
+    private String industryRealm;
+
+}
