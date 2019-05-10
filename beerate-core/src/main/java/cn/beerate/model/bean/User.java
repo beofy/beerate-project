52a392077043837396f028bd32d7b398ecb56ba4
minxiang <294315568@qq.com>
2019-05-10 16:55:29 +0800

    update 用户对象

--- beerate-core/src/main/java/cn/beerate/model/bean/User.java
+++ beerate-core/src/main/java/cn/beerate/model/bean/User.java
@@ -1,27 +1,51 @@
 package cn.beerate.model.bean;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@Getter
+@Data
 public class User {
-    /** 用户ID */
-    private long id;
 
-    /** 用户名称 */
+    /**
+     * 授权令牌
+     */
+    private String token;
+
+    /**
+     * 用户名称
+     */
     private String name;
 
-    /** 用户头像 */
+    /**
+     * 用户头像
+     */
     private String photo;
-    
-    /** 是否认证 */
+
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 是否认证
+     */
     private boolean isApprove = false;
 
-    public User(long id, String name, String photo, boolean isApprove) {
-        this.id = id;
+    public User(String token, String name, String photo, String mobile, String email, boolean isApprove) {
+        this.token = token;
         this.name = name;
         this.photo = photo;
+        this.mobile = mobile;
+        this.email = email;
         this.isApprove = isApprove;
     }
+
+
 }
