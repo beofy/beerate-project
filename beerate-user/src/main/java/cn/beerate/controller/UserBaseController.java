48c8272acfeea271f168139dfdd1f1357bf49695
minxiang <294315568@qq.com>
2019-05-10 16:56:12 +0800

    更新用户基础控制器

--- beerate-user/src/main/java/cn/beerate/controller/UserBaseController.java
+++ beerate-user/src/main/java/cn/beerate/controller/UserBaseController.java
@@ -1,26 +1,30 @@
 package cn.beerate.controller;
 
+import cn.beerate.PropertiesHolder;
 import cn.beerate.constant.SessionKey;
 import cn.beerate.model.bean.User;
+import cn.beerate.security.Encrypt;
 
 public class UserBaseController extends BaseController{
 
     /**
-     * 获取当前登录管理员
+     * 获取当前用户
      */
     public User getUser(){
         return getSessionAttr(SessionKey.USER_SESSION_KEY,User.class);
     }
 
     /**
-     * 获取当前管理员id
+     * 获取当用户id
      */
     long getUserId(){
-        return getUser().getId();
+        String userId = Encrypt.decrypt3DES(getUser().getToken(),PropertiesHolder.properties.getSecurityProperties().getDes_encrypt_key());
+
+        return Long.parseLong(userId);
     }
 
     /**
-     * 获取管理员用户名
+     * 获取用户名
      */
     public String getUserName(){
         return getUser().getName();
