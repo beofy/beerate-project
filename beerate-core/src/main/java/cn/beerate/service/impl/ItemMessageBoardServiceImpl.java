49ae50f5e8902941332a36367c9dc54e8f5a4276
minxiang <294315568@qq.com>
2019-05-16 15:22:11 +0800

    项目留言实现类

--- beerate-core/src/main/java/cn/beerate/service/impl/ItemMessageBoardServiceImpl.java
+++ beerate-core/src/main/java/cn/beerate/service/impl/ItemMessageBoardServiceImpl.java
@@ -3,7 +3,11 @@ package cn.beerate.service.impl;
 import cn.beerate.dao.ItemMessageBoardDao;
 import cn.beerate.model.entity.t_item_message_board;
 import cn.beerate.service.ItemMessageBoardService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional(readOnly = true)
 public class ItemMessageBoardServiceImpl extends BaseServiceImpl<t_item_message_board> implements ItemMessageBoardService {
     private ItemMessageBoardDao itemMessageBoardDao;
     public ItemMessageBoardServiceImpl(ItemMessageBoardDao itemMessageBoardDao) {
