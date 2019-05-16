ecb85c160118190139bd7c6bf09cab2bde02ffe4
minxiang <294315568@qq.com>
2019-05-16 15:24:43 +0800

    项目方 Controller 和 Service

--- beerate-core/src/main/java/cn/beerate/service/ProjectorService.java
+++ beerate-core/src/main/java/cn/beerate/service/ProjectorService.java
@@ -1,4 +1,10 @@
 package cn.beerate.service;
 
+import cn.beerate.common.Message;
+import cn.beerate.model.dto.ProjectorList;
+import org.springframework.data.domain.Page;
+
 public interface ProjectorService {
+
+     Message<Page<ProjectorList>> pageOfProjectList(int page, int size, String column, String order);
 }
--- beerate-user/src/main/java/cn/beerate/controller/ProjectorController.java
+++ beerate-user/src/main/java/cn/beerate/controller/ProjectorController.java
@@ -1,21 +1,54 @@
 package cn.beerate.controller;
 
-import cn.beerate.service.UserBusinessService;
+import cn.beerate.common.Message;
+import cn.beerate.service.ProjectorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * 项目方控制器
+ */
 @RestController
 @RequestMapping("/user/projector")
 public class ProjectorController {
 
-    private UserBusinessService userBusinessService;
-
-    public ProjectorController(UserBusinessService userBusinessService) {
-        this.userBusinessService = userBusinessService;
+    private ProjectorService projectorService;
+    public ProjectorController(ProjectorService projectorService) {
+        this.projectorService = projectorService;
     }
 
 
+    /**
+     * 项目列表
+     * @return
+     */
     @GetMapping("/list")
-    public Message<>
+    public Message list(){
+        return null;
+    }
+
+    /**
+     * 项目简介
+     */
+    @PostMapping("/intro")
+    public Message intro(){
+        return null;
+    }
+
+
+    /**
+     * @// TODO: 2019/5/16  项目方功能待完善 
+     */
+
+
+    /**
+     * 项目详细信息
+     */
+    @PostMapping("/detail")
+    public Message detail(){
+        return null;
+    }
 }
