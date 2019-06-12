b0779a510564d44552866d513870be756ec1bec9
minxiang <294315568@qq.com>
2019-06-12 10:03:24 +0800

    update

--- src/main/java/cn/beerate/service/ItemLoanService.java
+++ src/main/java/cn/beerate/service/ItemLoanService.java
@@ -1,7 +1,6 @@
 package cn.beerate.service;
 
 import cn.beerate.common.Message;
-import cn.beerate.model.AuditStatus;
 import cn.beerate.model.entity.t_item_loan;
 
 public interface ItemLoanService extends ItemCommonService<t_item_loan>{
@@ -11,10 +10,5 @@ public interface ItemLoanService extends ItemCommonService<t_item_loan>{
     Message<t_item_loan> addItemLoanByUser(t_item_loan itemLoan, long userId);
 
     Message<t_item_loan> addItemLoanByAdmin(t_item_loan itemLoan, long adminId);
-
-    /**
-     * 项目审核
-     */
-    Message<t_item_loan> auditItemLoan(AuditStatus auditStatus,long itemId);
 
 }
